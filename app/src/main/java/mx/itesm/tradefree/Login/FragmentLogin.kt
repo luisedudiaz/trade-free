3c41215815bdb60e170d5a3c06effc1f7d7e3dea
Luis Díaz <luis.edu.diaz@outlook.com>
2019-10-02 14:51:29 -0500

    Transition Login to Register

--- app/src/main/java/mx/itesm/tradefree/Login/FragmentLogin.kt
+++ app/src/main/java/mx/itesm/tradefree/Login/FragmentLogin.kt
@@ -1,12 +1,17 @@
 package mx.itesm.tradefree.Login
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.lifecycle.Observer
 import mx.itesm.tradefree.R
+import mx.itesm.tradefree.Register.ActivityRegister
 
 
 class FragmentLogin : Fragment() {
@@ -19,6 +24,18 @@ class FragmentLogin : Fragment() {
     ): View? {
         viewModelLogin = ViewModelProviders.of(this).get(ViewModelLogin::class.java)
         val root = inflater.inflate(R.layout.fragment_login, container, false)
+        val btnRegister: Button = root.findViewById(R.id.btnRegistrate)
+        btnRegister.setOnClickListener {
+            val intent = Intent(context, ActivityRegister::class.java)
+            viewModelLogin.sendMessageToA("D")
+            startActivity(intent)
+            /** This line is for delete an activity from the stack.
+             *
+             * activity?.finish()
+             *
+             */
+
+        }
         return root
     }
 
