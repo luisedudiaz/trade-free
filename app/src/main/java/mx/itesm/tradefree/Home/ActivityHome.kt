5255d2687f099997eb5412caef729a41cfe6a6ed
Luis Díaz <luis.edu.diaz@outlook.com>
2019-10-03 10:44:49 -0500

    Floating button home activity

--- app/src/main/java/mx/itesm/tradefree/Home/ActivityHome.kt
+++ app/src/main/java/mx/itesm/tradefree/Home/ActivityHome.kt
@@ -1,32 +1,40 @@
 package mx.itesm.tradefree.Home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_home.*
+import mx.itesm.tradefree.Login.ActivityLogin
 import mx.itesm.tradefree.R
+import mx.itesm.tradefree.Register.ActivityRegister
 
 class ActivityHome : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.settings_menu, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.profile_menu -> true
-            R.id.logout_menu -> true
-
+            R.id.logout_menu -> {
+                val intent = Intent(this, ActivityLogin::class.java)
+                startActivity(intent)
+                return true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
