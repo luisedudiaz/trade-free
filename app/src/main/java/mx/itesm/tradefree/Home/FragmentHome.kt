c96617b8b90d5b1f487e64aa3805450dddd7fac8
Daniela Colin <a01371734@itesm.mx>
2019-10-03 13:30:26 -0500

    Fragment Home

--- app/src/main/java/mx/itesm/tradefree/Home/FragmentHome.kt
+++ app/src/main/java/mx/itesm/tradefree/Home/FragmentHome.kt
@@ -1,13 +1,17 @@
 package mx.itesm.tradefree.Home
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import mx.itesm.tradefree.Login.ViewModelLogin
 
 import mx.itesm.tradefree.R
+import mx.itesm.tradefree.Register.ActivityRegister
 
 class FragmentHome : Fragment() {
 
@@ -20,5 +24,7 @@ class FragmentHome : Fragment() {
         viewModelHome = ViewModelProviders.of(this).get(ViewModelHome::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         return root
+
     }
+
 }
